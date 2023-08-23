f3023945da047c745a240278d490596eda99cb65
Rodrigo Rafael <rodrigo.sousa-1@syngenta.com>
2023-08-23 10:23:13 -0300

    add plop files

---
+++ src/pages/dashboard/dashboard.container.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './dashboard.container.styles.less';
+
+const Dashboard: React.FC = () => {
+
+return (
+    <div className='dashboard-container'>
+
+    </div>
+);
+};
+
+export default Dashboard;
